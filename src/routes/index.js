58a1f8c77a9b8512e1a5577a223c440d174a2632
Atittaya27 <147577320+Atittaya27@users.noreply.github.com>
2025-03-08 01:43:10 +0700

    add routes product&program

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router()
 const customerRouter = require('./customer')
+const programRouter = require('./program')
+const productRouter = require('./product')
+
 
 // Routes
 router.get('/', (req, res) => res.send('API V1'))
 router.use('/customer', customerRouter)
+router.use('/program', programRouter)
+router.use('/product', productRouter)
 
 module.exports = router
