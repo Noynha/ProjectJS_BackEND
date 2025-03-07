2f3df239fbe43124afb3b0577ebdce9e1d838c0c
nannysuchawadi <147577225+nannysuchawadi@users.noreply.github.com>
2025-03-07 15:29:24 +0700

    add table orders

--- src/db/init-db.js
+++ src/db/init-db.js
@@ -18,3 +18,26 @@ db.run(`
       }
     }
 );
+
+// Create table order
+db.run(`
+  CREATE TABLE IF NOT EXISTS orders (
+    orders_id binary(16) PRIMARY KEY,
+    customer_id binary(16),
+    total_price float(20),
+    status varchar(40),
+    drop_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    take_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+  )
+`, 
+    [],
+    function(error) {
+      if (error) {
+        throw new Error(error);
+      }
+    }
+);
+
+
