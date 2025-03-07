3bf57c177cba39a3c8c0da3364161b3eb718e0b1
Noynha <147577227+Noynha@users.noreply.github.com>
2025-03-07 15:17:03 +0700

    add product

--- src/db/init-db.js
+++ src/db/init-db.js
@@ -5,8 +5,26 @@ const db = new sqlite3.Database('./src/db/database.db');
 db.run(`
   CREATE TABLE IF NOT EXISTS customer (
     customer_id binary(16) PRIMARY KEY,
-    customer_name varchar(255),
+    customer_name varchar(40),
     customer_phone varchar(10),
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+  )
+`, 
+    [],
+    function(error) {
+      if (error) {
+        throw new Error(error);
+      }
+    }
+);
+
+// Create table product
+db.run(`
+  CREATE TABLE IF NOT EXISTS product (
+    product_id binary(16) PRIMARY KEY,
+    product_type varchar(40),
+    product_price float(20),
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP
   )
