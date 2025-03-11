b9e5b964fde6c6a900e473dc181e94759bef68f0
csnpcjexpress <84871549+csnpch@users.noreply.github.com>
2025-03-12 03:06:12 +0700

    Revert "[Add] env"
    This reverts commit 38d1e892c162c3c84b403b058d6cbe94b67b817e.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const router = require('./src/routes')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = 5000
 
 app.use(express.json())
 app.use(cors())
